2e372d72e99964bb8cc393c4956f43dd1addaf68
Aji Guna <gunaitsugo@gmail.com>
2023-02-23 09:24:33 +0700

    hello response

---
+++ index.js
@@ -0,0 +1,36 @@
+// Dependencies
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const port = 6000;
+const url = 'https://api.telegram.org/bot';
+const apiToken = '6236117988:AAEQKnAENQW9j4dmuF1SvENBGuhHsrZslYQ';
+// Configurations
+app.use(bodyParser.json());
+// Endpoints
+app.post('/', (req, res) => {
+     // console.log(req.body);
+     const chatId = req.body.message.chat.id;
+     const sentMessage = req.body.message.text;
+     // Regex for hello
+     if (sentMessage.match(/hello/gi)) {
+          axios.post(`${url}${apiToken}/sendMessage`,
+               {
+                    chat_id: chatId,
+                    text: 'hello back 👋'
+               })
+               .then((response) => { 
+                    res.status(200).send(response);
+               }).catch((error) => {
+                    res.send(error);
+               });
+     } else {
+          // if no hello present, just respond with 200 
+          res.status(200).send({});
+     }
+});
+// Listening
+app.listen(port, () => {
+     console.log(`Listening on port ${port}`);
+});
